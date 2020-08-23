3db7b49b68d19eb93e4cd48184bebcd4773e7527
andriy999000 <vladRydenko77Agmail.com>
2020-08-23 13:24:45 +0300

    commit 2.0

--- src/com/google/Main.java
+++ src/com/google/Main.java
@@ -1,5 +1,9 @@
 package com.google;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
@@ -13,6 +17,19 @@ public class Main {
         byte b = 127;
         b++;
         System.out.println(b);
+
+
+//        Задание 4
+//        Написать тернарную операцию, которая определяет четность числа и формирует
+//        соответствующую строку, которая выводится на экран
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\n\nВведите любое число:");
+        int number = scanner.nextInt();
+        int i = number % 2;
+
+        System.out.println(i == 0 ? "Ваше число чётное!" : "Ваше число не чётное!");
+
 
 
 
